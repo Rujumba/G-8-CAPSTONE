385801ce8985eb4e6bd788a77b5ae10cfe1e8e29
Mwine <amumpairetumuramye09@gmail.com>
2024-04-13 17:58:33 +0300

    rebase

--- IFHMS-MEDICARE/repository/accountRepositories/GeneralLedgerRepository.java
+++ IFHMS-MEDICARE/repository/accountRepositories/GeneralLedgerRepository.java
@@ -4,8 +4,7 @@ package repository.accountRepositories;
 import models.account.GeneralLedger;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+ 
 import java.util.Map;
 
 public class GeneralLedgerRepository {
@@ -38,29 +37,17 @@ public class GeneralLedgerRepository {
 
         String id = generalLedger.getId();
 
+        String filename = "GeneralLedger2.dat";
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename, true))) {
+            oos.writeObject(generalLedger);
+            System.out.println("GeneralLedger object saved successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    /**
-     * Returns all general ledger entries for the file and stores them in a list
-     */
-    // public List<GeneralLedger> getAllGeneralLedgerEntries() {
-
-    //     String filename = "GeneralLedger2.dat";
-    //     try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-    //         GeneralLedger generalLedger;
-    //         while ((generalLedger = (GeneralLedger) ois.readObject()) != null) {
-    //             generalLedgerList.add(generalLedger);
-    //         }
-    //     } catch (EOFException e) {
-    //         e.printStackTrace();
-    //     } catch (IOException | ClassNotFoundException e) {
-    //         e.printStackTrace();
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-
-    //     return generalLedgerList;
-    // }
+ 
 
 }
